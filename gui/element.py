a022c6a4b9d8948ca0f2131445adfffe9a997ee5
Moppi1 <braunjamie1@gmail.com>
2024-07-26 17:15:37 +0200

    update

--- gui/element.py
+++ gui/element.py
@@ -204,11 +204,108 @@ class button:
         self.marker_hover_color     = marker_hover_color if marker_hover_color != None else self.marker_hover_color
         self.text_hover_color       = text_hover_color if text_hover_color != None else self.text_hover_color
 
-    def set_pos(self,pos):
-        """pos has to be a value between 0 and 1"""
-        self.pos = pos
     def get_name(self):
         """returns the name of the slider"""
         return self.name
     def get_value(self):
         return self.value
+    def set_pos(self,pos):
+        """pos has to be a value between 0 and 1"""
+        self.pos = pos
+
+class togglebutton:
+    def __init__(self,name,pos:v.vec,width:float,text:str=None,call_function=None) -> None:
+        """creates a 'toggle button' which bool value can be toggled by clicking"""
+
+        self.pos        = pos
+        self.text       = text if text != None else name
+        self.name       = name
+        self.width      = width             # for collision purposes
+        self.height     = 50                # for collision static
+        self.function   = call_function
+
+        # interaction
+
+        self.hover = False
+        self.time_hover_begin = None
+        self.pressed = False
+
+        # visuals
+
+        self.text_color             = (207, 204, 204)
+        self.marker_hover_color     = (212, 161, 21)
+        self.text_hover_color       = (150,150,150)
+
+    def update(self):
+        """updates the button (mouse input). has to be called each frame"""
+
+        mouse = v.vec()
+        mouse.from_list(pygame.mouse.get_pos())
+
+        def check_collision():
+            nonlocal mouse
+            pos = self.pos.pyg_center()
+
+            if mouse.x >= pos.x and mouse.x <= pos.x + self.width: #check x collision
+                if mouse.y >= pos.y and mouse.y <= pos.y + self.height: # check y collision 
+                    return True
+
+            return False
+
+
+        if check_collision():
+            if pygame.mouse.get_pressed()[0]: # clicked
+                self.pressed = True
+            else:   # hover
+                self.pressed = False
+                if not self.hover:
+                    self.time_hover_begin = pygame.time.get_ticks()
+                self.hover   = True
+        else: # not touched
+            self.pressed   = False
+            self.hover      = False
+        
+        # == does the logic == 
+        if self.pressed: 
+            if self.function != None:
+                    self.function()
+                    return
+
+            self.value = True if self.pressed else False
+
+    def render(self):
+        global text
+        pos = self.pos.pyg_center()
+        
+        if self.hover:
+            time = pygame.time.get_ticks() - self.time_hover_begin
+            animation_value = (time/100)**3
+            if animation_value >= 1 : animation_value = 1
+
+            pygame.draw.rect(pygame.display.get_surface(),self.marker_hover_color,(pos.x,pos.y+46-(40*animation_value),6,40*animation_value),0,4)
+            pygame.display.get_surface().blit(text.render(self.text,True,self.text_hover_color),pos.ret_add(v.vec(10*animation_value,0)).to_list()) #,
+        
+        else:
+            pygame.display.get_surface().blit(text.render(self.text,True,self.text_color),pos.ret_add(v.vec()).to_list())
+            
+    def change_visuals(self,text_color=None,marker_hover_color=None,text_hover_color=None):
+        """change the values of the visual aspects of the sliders"""
+
+        self.text_color             = text_color if text_color != None else self.text_color
+        self.marker_hover_color     = marker_hover_color if marker_hover_color != None else self.marker_hover_color
+        self.text_hover_color       = text_hover_color if text_hover_color != None else self.text_hover_color
+
+    def get_name(self):
+        """returns the name of the slider"""
+        return self.name
+    def get_value(self):
+        return self.value
+    def set_pos(self,pos):
+        """pos has to be a value between 0 and 1"""
+        self.pos = pos
+
+class inputfield:
+    pass
+
+class loadingscreen:
+    pass
